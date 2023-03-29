cff51e3169a5fc2222d2b1db5ad81d895f82c91b
Vladyslav Nikitchuk <nikit4uk@gmail.com>
2023-03-29 19:45:11 +0300

    add social link

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -3,25 +3,7 @@ import Image from 'next/image'
 export default function SocialIcons() {
     return (
         <>
-            <a href=''>
-                <Image 
-                    src="/social-icon/facebook.svg"
-                    alt="facebook-icon"
-                    width={32}
-                    height={32}
-                    priority
-                />
-            </a>
-            <a href=''>
-                <Image 
-                    src="/social-icon/instagram.svg"
-                    alt="instagram-icon"
-                    width={32}
-                    height={32}
-                    priority
-                />
-            </a>
-            <a href=''>
+            <a href='https://telegram.me/agtagent' target='_blanc'>
                 <Image 
                     src="/social-icon/telegram.svg"
                     alt="telegram-icon"
@@ -30,7 +12,7 @@ export default function SocialIcons() {
                     priority
                 />
             </a>
-            <a href=''>
+            <a href='https://wa.me/447488818682' target='_blanc'>
                 <Image 
                     src="/social-icon/whatsapp.svg"
                     alt="whatsapp-icon"
@@ -39,7 +21,25 @@ export default function SocialIcons() {
                     priority
                 />
             </a>
-            <a href=''>
+            <a href='https://www.facebook.com/profile.php?id=100075950215275' target='_blanc'>
+                <Image 
+                    src="/social-icon/facebook.svg"
+                    alt="facebook-icon"
+                    width={32}
+                    height={32}
+                    priority
+                />
+            </a>
+            <a href='https://instagram.com/aerospaceglobaltrade?igshid=YmMyMTA2M2Y=' target='_blanc'>
+                <Image 
+                    src="/social-icon/instagram.svg"
+                    alt="instagram-icon"
+                    width={32}
+                    height={32}
+                    priority
+                />
+            </a>
+            <a href='https://twitter.com/agtagent' target='_blanc'>
                 <Image 
                     src="/social-icon/twitter.svg"
                     alt="twitter-icon"
